84309f18ce15f1312714c20f413a224fc701dcea
Oscar <Oscar@LAPTOP-Q7G67LI0>
2024-03-24 23:00:39 +0100

    cambio de variable 'def' por 'fin'

--- src/Git/Git.java
+++ src/Git/Git.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 public class Git {
 	//declaramos la variables que nos hacen falta
 	double uf1, uf2, uf3;
-	double acu1, acu2, acu3, def;
+	double acu1, acu2, acu3, fin;
 	//utilizames ecaner para poder introducir datos
 	Scanner entrada = new Scanner(System.in);
 
@@ -67,7 +67,7 @@ public class Git {
 		acu2 = uf2 * 0.35;
 		acu3 = uf3 * 0.30;
 		
-		def = acu1 + acu2+ acu3;
+		fin = acu1 + acu2+ acu3;
 		
 		//hasta aqui la tenemos calculada peor no la mostramos
 	}
@@ -82,16 +82,16 @@ public class Git {
 		System.out.println(" acumuado 2 = "+ acu2);
 		System.out.println(" acumuado 3 = "+ acu3);
 		
-		System.out.println(" nota definitiva es = "+ def);
+		System.out.println(" nota definitiva es = "+ fin);
 		
 	}
 
 	public void aprobado() {
 			
-			if(def<5 && def>=0) {
+			if(fin<5 && fin>=0) {
 				System.out.println("suspendio");
 			}else {
-				if (def>=5 && def<=10 ) {
+				if (fin>=5 && fin<=10 ) {
 				System.out.println("aprobado");
 				}else {
 					System.out.println(" error en la notas");
